dd14e3b466b339cd6ffb3c855e9f965535909cd0
Sriprasanth8 <59812016+Sriprasanth8@users.noreply.github.com>
2020-06-11 16:38:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+for (var i = 0; i < names.length; i++) {
+    var name = names[i];
+    var first = name.charAt(0);
+    var lower = first.toLowerCase();
+    if (lower == "j") {
+      byeSpeaker.speak(name);
+    } else {
+     helloSpeaker.speak(name);
+    }
+} 
